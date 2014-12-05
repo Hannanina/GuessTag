909dea0a84a8fca2234019316ee366191ee2886b
Hannanina <hannaninafrank@gmail.com>
2014-12-05 16:36:48 +0100

    SelectTaggerActivity and InputTagActivity added
    let's give it another try :)

---
+++ GuessTag/src/com/example/guesstag/SelectTaggerActivity.java
@@ -0,0 +1,42 @@
+package com.example.guesstag;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SelectTaggerActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_select_tagger);
+	}
+	
+	public void selectTagger(){
+		//toDo: disable button for other players and bring them to guessTag Activity
+		Intent intent = new Intent(this, InputTagActivity.class);
+		startActivity(intent);
+	
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.select_tagger, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
