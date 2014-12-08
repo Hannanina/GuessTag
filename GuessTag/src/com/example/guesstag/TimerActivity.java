8bfda18b42c6ab27b09fb89439d0f20c69b53aa6
Hannanina <hannaninafrank@gmail.com>
2014-12-08 13:44:43 +0100

    TimerActivity and first version of timer added

--- GuessTag/src/com/example/guesstag/TimerActivity.java
+++ GuessTag/src/com/example/guesstag/TimerActivity.java
@@ -1,18 +1,49 @@
 package com.example.guesstag;
 
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class TimerActivity extends ActionBarActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_timer);
-	}
+	ProgressBar mProgressBar;
+	CountDownTimer mCountDownTimer;
+	int i=1;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+       
+    	super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_timer);
+		
+		mProgressBar=(ProgressBar)findViewById(R.id.circularProgressbar);
+		mProgressBar.setProgress(i);
+		mCountDownTimer=new CountDownTimer(30000,300) {
+
+		        @Override
+		        public void onTick(long millisUntilFinished) {
+		    		TextView textview = (TextView)findViewById(R.id.textView1);
+		    		textview.setText("seconds remaining: " + millisUntilFinished / 1000);
+		            i++;
+		            mProgressBar.setProgress(i);
+
+		        }
+
+		        @Override
+		        public void onFinish() {
+		    		TextView textview = (TextView)findViewById(R.id.textView1);
+		    		textview.setText("Time is up");
+		    		
+		        }
+		    };
+		    mCountDownTimer.start();
+    }
+    
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
